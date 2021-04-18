3e5e702f3484539a941f782f1c8f15a8a7a99118
RONALD EDUARDO EDUARDO <>
2021-04-18 21:42:20 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int lustro=5;
+  int segundos;
+
+  segundos=lustro*365*24*60*60;
+  cout<<"en un lustro hay " <<segundos<< " segundos";
+}
