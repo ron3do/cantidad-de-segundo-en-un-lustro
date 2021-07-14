e7aae11e36b01403fd3f7a4babef86cbba0afc91
RONALD EDUARDO EDUARDO <82458164+ron3do@users.noreply.github.com>
2021-07-14 01:17:20 +0000

    kjsdasd

--- main.cpp
+++ main.cpp
@@ -1,11 +1,14 @@
 #include <iostream>
-
+// comentario 1
 using namespace std;
 
 int main() {
   int lustro=5;
   int segundos;
+  int casa=0;
 
   segundos=lustro*365*24*60*60;
   cout<<"en un lustro hay " <<segundos<< " segundos";
+  casa=casa+1;
+
 }
